84fff198d6c7072fc491d21864e383a58dfdd97c
Lynx <lynx@ilynxcat.me>
2025-12-22 10:25:36 -0800

    add base PlaceholderAPI integration

--- src/main/java/org/modernbeta/admintoolbox/integration/placeholderapi/PlaceholderAPIIntegration.java
+++ src/main/java/org/modernbeta/admintoolbox/integration/placeholderapi/PlaceholderAPIIntegration.java
@@ -1,4 +1,20 @@
 package org.modernbeta.admintoolbox.integration.placeholderapi;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.modernbeta.admintoolbox.AdminToolboxPlugin;
+import org.modernbeta.admintoolbox.integration.placeholderapi.expansion.StreamerModePlaceholder;
+
 public class PlaceholderAPIIntegration {
+	private final AdminToolboxPlugin plugin;
+
+	private final StreamerModePlaceholder streamerModePlaceholder;
+
+	public PlaceholderAPIIntegration(AdminToolboxPlugin plugin) {
+		this.plugin = plugin;
+		this.streamerModePlaceholder = new StreamerModePlaceholder(plugin);
+	}
+
+	public boolean registerPlaceholders() {
+		return this.streamerModePlaceholder.register();
+	}
 }
